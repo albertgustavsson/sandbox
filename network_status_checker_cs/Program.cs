ed6e99a019f6ca49d96a50e23f98cef9c536de49
Albert Gustavsson <albert99gustavsson@gmail.com>
2022-01-08 15:34:24 +0100

    Added network status program in c#

---
+++ network_status_checker_cs/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+if (!System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
+    Console.WriteLine("Not connected to network");
+
+string hostName = Dns.GetHostName();
+Console.WriteLine($"Hostname: {hostName}");
+
+IPHostEntry ipEntry = Dns.GetHostEntry(hostName, AddressFamily.InterNetwork);
+
+try
+{
+    Console.WriteLine($"IPv4 address: {ipEntry.AddressList[0]}");
+}
+catch (IndexOutOfRangeException)
+{
+    Console.Error.WriteLine("No IP address found");
+}
